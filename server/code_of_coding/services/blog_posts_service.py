269f77b74ab006af8213d3e7e7a60c7f12f1ba38
John Caldas <johntcaldas@yahoo.com>
2014-09-07 19:11:04 -0400

    Begin building posts service.

--- server/code_of_coding/services/blog_posts_service.py
+++ server/code_of_coding/services/blog_posts_service.py
@@ -2,17 +2,38 @@
 Blog Posts Service
 
 """
-#import pymongo
+from pymongo import MongoClient
+import datetime
+
 
 class BlogPostsService():
 
-    def get_blog_posts(self):
-        """
-        Write Me.
-        """
+    def __init__(self):
+        db_client = MongoClient('localhost', 27017)
+        db = db_client.code_of_coding
+        self.posts = db.posts
 
-        posts = {
-            "test_key": "test value"
+    def get_posts(self):
+        posts_cursor = self.posts.find()
+        posts_list = posts_cursor[:]
+        return posts_list
+
+    def add_post(self, html, tags, author, date=datetime.datetime.utcnow()):
+        post = {
+            "html": html,
+            "tags": tags,
+            "author": author,
+            "date": date
         }
+        blog_post_id = self.posts.insert(post)
+        return blog_post_id
 
-        return posts
+    def update_post(self, post_id, html, tags, author, date):
+        self.posts.update({"_id": post_id}, {
+            "html": html,
+            "tags": tags,
+            "author": author,
+            "date": date
+        })
+
+
