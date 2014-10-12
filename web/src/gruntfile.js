5a3c61227834dae735551cf38e88dae20fe050ce
John Caldas <johntcaldas@gmail.com>
2014-10-12 09:14:27 -0400

    Add JSHint task to gruntfile.

--- web/src/gruntfile.js
+++ web/src/gruntfile.js
@@ -1,8 +1,10 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        pkg : grunt.file.readJSON('package.json'),
         dirs: {
           handlebars_src: 'handlebars/*.handlebars',
-          handlebars_dest: '../js/handlebars_templates.js'
+          handlebars_dest: '../js/handlebars_templates.js',
+          js: '../js/**/*.js'
         },
         watch: {
           handlebars: {
@@ -18,12 +20,21 @@ module.exports = function(grunt) {
             src: '<%= dirs.handlebars_src %>',
             dest: '<%= dirs.handlebars_dest %>'
           }
+        },
+        jshint : {
+            files : '<%= dirs.js %>',
+            options : {
+                jshintrc: '.jshintrc',
+                globals : {
+                    jQuery : true
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', 'handlebars');
-
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
+    grunt.registerTask('default', ['handlebars', 'jshint']);
 };
